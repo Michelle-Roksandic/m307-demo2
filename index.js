762562c0da9a04d7f0d2a003660e949ddeafd8a3
Jujuubahn <168190918+julien-juju@users.noreply.github.com>
2024-11-05 10:52:25 +0000

    ca funktionerendes Login 05.11

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { createApp } from "./config.js";
 
 const app = createApp({
-  user: "autumn_star_7622",
-  host: "168.119.168.41",
-  database: "demo",
-  password: "uaioysdfjoysfdf",
-  port: 18324,
+  user: "wandering_water_3730",
+  host: "bbz.cloud",
+  database: "wandering_water_3730",
+  password: "4cc7ed3614d24ef81c834e37c374892d",
+  port: 30211,
 });
 
 /* Startseite */
@@ -17,13 +17,43 @@ app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
 });
 
-app.get("/feed", async function (req, res) {
-  res.render("feed", {});
+app.get("/", async function (req, res) {
+  const posts = await app.locals.pool.query("SELECT * FROM posts");
+  res.render("start", { posts: posts.rows });
+});
+
+app.get("/post/:id", async function (req, res) {
+  const posts = await app.locals.pool.query(
+    `select * from posts WHERE id = ${req.params.id}`
+  );
+  res.render("post", { posts: posts.rows });
 });
 
 app.get("/user", async function (req, res) {
-  res.render("user", {});
+  const posts = await app.locals.pool.query(
+    "SELECT * FROM posts WHERE user_id = $1",
+    [req.session.userid]
+  );
+  res.render("user", { posts: posts.rows });
 });
+
+app.get("/new_post", async function (req, res) {
+  if (!req.session.userid) {
+    res.redirect("/login");
+    return;
+  }
+  res.render("new_post", {});
+});
+
+app.post("/create_post", async function (req, res) {
+  await app.locals.pool.query(
+    "INSERT INTO posts (title, content, user_id) VALUES ($1, $2, $3)"[
+      (req.body.title, req.body.content, req.session.userid)
+    ]
+  );
+  res.direct("/feed");
+});
+
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
   console.log(`Example app listening at http://localhost:3010`);
